c7c67b69a967cb07e2aefe6b81565d9e2b272473
Humberto Navarro <a320669@uach.mx>
2023-04-24 17:16:36 -0600

    Add barebones component and structure main page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import styles from "./style";
+
+import {Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer} from "./components";
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          stats
+          business
+          billing
+          carddeal
+          testimonials
+          clients
+          cta
+          footer
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+/* We import and export all of our components here to improve code readability and to decrease the number of import lines in our main app */
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
