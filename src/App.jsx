ab9afa56e84d7282f4b9611c5a0366f732d0cd09
Humberto Navarro <a320669@uach.mx>
2023-04-30 13:56:04 -0600

    Add responsive navbar side menu

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,26 @@ export default function App() {
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          stats
-          business
-          billing
-          carddeal
-          testimonials
-          clients
-          cta
-          footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
     </div>
